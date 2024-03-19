1314b1e0c191fedc028def25b77124e968a6ac54
dalen01 <danda@hotmail.no>
2024-03-19 22:28:57 +0100

    Update Functions.cpp and Functions.h, add MazeWalls.h

--- Functions.h
+++ Functions.h
@@ -1,1 +1,64 @@
 #pragma once
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+bool checkButtonPressed(const int buttonPin, bool &buttonState, bool &lastButtonState, unsigned long long &lastDebounceTime){
+    const int debounceDelay = 50;
+
+    bool reading = !digitalRead(buttonPin);
+    if (reading != lastButtonState) {
+        lastDebounceTime = millis();
+    }
+    if ((millis() - lastDebounceTime) > debounceDelay){
+        if (reading != buttonState){
+            buttonState = reading;
+            if (buttonState == HIGH){
+                return true;
+            }
+        }
+    }
+    lastButtonState = reading;
+    return false;
+}
+bool allowedMove(int currentPos, int direction){
+    int matrixSize = 8;
+    int newPos = currentPos + direction;
+    if (direction == 1 && currentPos % matrixSize == 7){
+        return false;
+    }
+    else if(direction == -1 && currentPos % matrixSize == 0){
+        return false;
+    }
+    else if(direction == 8 && currentPos >= matrixSize*(matrixSize-1)){
+        return false;
+    }
+    else if(direction == -8 && currentPos < matrixSize){
+        return false;
+    }
+    return true;
+}
+void moveLight(Adafruit_NeoPixel &matrix,int &currentPos, int direction, int red = 255, int green = 0, int blue = 0, int step = 1){
+    // up = 8, down = -8, left = 1, right = -1
+    int matrixSize = matrix.numPixels(); //assuming the matrix is square
+    matrixSize = sqrt(matrixSize);
+    int newPos = currentPos + direction*step;
+    if (direction == 1 && currentPos % matrixSize == 7){
+        newPos = currentPos;
+        Serial.println("At left edge");
+    }
+    else if(direction == -1 && currentPos % matrixSize == 0){
+        newPos = currentPos;
+        Serial.println("At right edge");
+    }
+    else if(direction == 8 && currentPos >= matrixSize*(matrixSize-1)){
+        newPos = currentPos;
+        Serial.println("At top edge");
+    }
+    else if(direction == -8 && currentPos < matrixSize){
+        newPos = currentPos;
+        Serial.println("At bottom edge");
+    }
+    matrix.setPixelColor(currentPos, 0, 0, 0);
+    matrix.setPixelColor(newPos, red, green, blue);
+    matrix.show();
+    currentPos = newPos;
+}
--- Maze/Maze.ino
+++ Maze/Maze.ino
@@ -1,6 +1,70 @@
-#include "../Functions.h"
+#include "C:\Users\danda\Documents\Geocaching\Utlegg\Maze Geocahce\Functions.h"
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+int randomNumber;
+const int numberOfButtons = 4;
+const int buttonPins[numberOfButtons] = {2, 3, 4, 5};
+bool lastButtonStates[numberOfButtons] = {0, 0, 0, 0};
+bool buttonStates[numberOfButtons] = {0, 0, 0, 0};
+unsigned long long lastDebounceTime[numberOfButtons] = {0, 0, 0, 0};
+bool buttonPressed[numberOfButtons] = {0, 0, 0, 0};
+int currentPos = 0;
+#define MATRIXPIN 6 
+#define NUMPIXELS 64
+
+Adafruit_NeoPixel matrix(NUMPIXELS, MATRIXPIN, NEO_GRB + NEO_KHZ800);
+
 void setup(){
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+clock_prescale_set(clock_div_1);
+#endif
+
+matrix.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+matrix.setBrightness(255);
+matrix.show(); // Initialize all pixels to 'off'
+
+for (int i=0;i<numberOfButtons;i++){
+    pinMode(buttonPins[i], INPUT_PULLUP);
+}
+randomSeed(analogRead(0));
+randomNumber = random(0, NUMPIXELS);
+
+Serial.begin(9600);
+Serial.println("Starting");
+matrix.setPixelColor(currentPos, 255, 0, 0);
+matrix.setPixelColor(randomNumber, 0, 255, 0);
+matrix.show();
 
 }
+
 void loop(){
+for (int i=0; i<numberOfButtons;i++){
+    buttonPressed[i] = checkButtonPressed(buttonPins[i], buttonStates[i], lastButtonStates[i], lastDebounceTime[i]);
+    if (buttonPressed[i]){
+        if (i == 0){
+            moveLight(matrix, currentPos, 1);
+        }
+        else if (i == 1){
+            moveLight(matrix, currentPos, 8);
+        }
+        else if (i == 2){
+            moveLight(matrix, currentPos, -1);
+        }
+        else if (i == 3){
+            moveLight(matrix, currentPos, -8);
+        }
+        if (currentPos == randomNumber){
+            Serial.println("You win!");
+            matrix.setPixelColor(randomNumber, 255, 0, 0);
+            matrix.show();
+            delay(1000);
+            randomNumber = random(0, NUMPIXELS);
+            matrix.setPixelColor(randomNumber, 0, 255, 0);
+            matrix.show();
+        }
+    }
+}
+
 }
