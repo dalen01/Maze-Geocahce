d7f9b21ca35e463adece7776a219441e50852ab3
dalen01 <danda@hotmail.no>
2024-03-21 09:07:22 +0100

    Add maze functionality and update maze walls

--- Functions.h
+++ Functions.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <math.h>
+#include "MazeWalls.h"
+int matrixSize = 8;
 bool checkButtonPressed(const int buttonPin, bool &buttonState, bool &lastButtonState, unsigned long long &lastDebounceTime){
     const int debounceDelay = 50;
 
@@ -19,46 +22,83 @@ bool checkButtonPressed(const int buttonPin, bool &buttonState, bool &lastButton
     lastButtonState = reading;
     return false;
 }
-bool allowedMove(int currentPos, int direction){
-    int matrixSize = 8;
-    int newPos = currentPos + direction;
-    if (direction == 1 && currentPos % matrixSize == 7){
+bool atEdge(int currentPos, int direction){
+    if (direction == 1 && currentPos % matrixSize == 6){
+        return true;
+    }
+    else if(direction == -1 && currentPos % matrixSize == 1){
+        return true;
+    }
+    else if(direction == 8 && currentPos >= matrixSize*(matrixSize-2)){
+        return true;
+    }
+    else if(direction == -8 && currentPos < matrixSize*2){
+        return true;
+    }
+    else{
         return false;
     }
-    else if(direction == -1 && currentPos % matrixSize == 0){
-        return false;
-    }
-    else if(direction == 8 && currentPos >= matrixSize*(matrixSize-1)){
-        return false;
-    }
-    else if(direction == -8 && currentPos < matrixSize){
-        return false;
-    }
-    return true;
 }
-void moveLight(Adafruit_NeoPixel &matrix,int &currentPos, int direction, int red = 255, int green = 0, int blue = 0, int step = 1){
+
+bool hitWall(int currentPos, int direction,int currentMaze){
+    if (direction == 1){
+        return wallsVetical[currentMaze][matrixSize-currentPos/matrixSize-1][matrixSize-currentPos%matrixSize-2];
+    }
+    else if (direction == -1){
+        return wallsVetical[currentMaze][matrixSize-currentPos/matrixSize-1][matrixSize-currentPos%matrixSize-1];
+    }
+    else if (direction == 8){
+        return wallsHorizontal[currentMaze][matrixSize-currentPos/matrixSize-2][matrixSize-currentPos%matrixSize-1];
+    }
+    else if (direction == -8){
+        return wallsHorizontal[currentMaze][matrixSize-currentPos/matrixSize-1][matrixSize-currentPos%matrixSize-1];
+    }
+}
+void moveLight(Adafruit_NeoPixel &matrix,int &currentPos, int direction,int currentMaze = 0, int red = 50, int green = 50, int blue = 50, int step = 1){
     // up = 8, down = -8, left = 1, right = -1
-    int matrixSize = matrix.numPixels(); //assuming the matrix is square
-    matrixSize = sqrt(matrixSize);
+    /*Serial.print("Current Position: ");
+    Serial.print(currentPos);
+    Serial.print("  Circle Position: 1: ");
+    Serial.print(circlePos[currentMaze][0]);
+    Serial.print("  2: ");
+    Serial.print(circlePos[currentMaze][1]);
+    Serial.print("  Direction: ");
+    Serial.print(direction);
+    Serial.print("  currentMaze: ");
+    Serial.println(currentMaze);*/
     int newPos = currentPos + direction*step;
-    if (direction == 1 && currentPos % matrixSize == 7){
+    if (atEdge(currentPos, direction)){
+        Serial.println("You hit the edge");
         newPos = currentPos;
-        Serial.println("At left edge");
     }
-    else if(direction == -1 && currentPos % matrixSize == 0){
+    else if(hitWall(currentPos, direction, currentMaze)){
+        Serial.println("You hit a wall");
         newPos = currentPos;
-        Serial.println("At right edge");
-    }
-    else if(direction == 8 && currentPos >= matrixSize*(matrixSize-1)){
-        newPos = currentPos;
-        Serial.println("At top edge");
-    }
-    else if(direction == -8 && currentPos < matrixSize){
-        newPos = currentPos;
-        Serial.println("At bottom edge");
     }
     matrix.setPixelColor(currentPos, 0, 0, 0);
+    if (currentPos == circlePos[currentMaze][0]){
+        matrix.setPixelColor(currentPos, 0, 50, 0);
+    }
+    else if (currentPos == circlePos[currentMaze][1]){
+        matrix.setPixelColor(currentPos, 0, 50, 0);
+    }
+    
     matrix.setPixelColor(newPos, red, green, blue);
-    matrix.show();
     currentPos = newPos;
+}
+void fillEdges(Adafruit_NeoPixel &matrix, int red = 0, int green = 1, int blue = 1){
+    for (int i=0; i<matrixSize; i++){
+        matrix.setPixelColor(i, red, green, blue);
+        matrix.setPixelColor(i+matrixSize*(matrixSize-1), red, green, blue);
+        matrix.setPixelColor(i*matrixSize, red, green, blue);
+        matrix.setPixelColor(i*matrixSize+matrixSize-1, red, green, blue);
+    }
+}
+
+int getPosition(int notSameAs = 0){
+    int position = random(0,matrixSize*matrixSize);
+    while (position == notSameAs || position< matrixSize || position >= matrixSize*(matrixSize-1) || position % matrixSize == 0 || position % matrixSize == matrixSize-1){
+        position = random(0,matrixSize*matrixSize);
+    }
+    return position;
 }
--- Maze/Maze.ino
+++ Maze/Maze.ino
@@ -3,14 +3,15 @@
 #ifdef __AVR__
 #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
-int randomNumber;
+int finalPosition;
+int currentMaze;
 const int numberOfButtons = 4;
 const int buttonPins[numberOfButtons] = {2, 3, 4, 5};
 bool lastButtonStates[numberOfButtons] = {0, 0, 0, 0};
 bool buttonStates[numberOfButtons] = {0, 0, 0, 0};
 unsigned long long lastDebounceTime[numberOfButtons] = {0, 0, 0, 0};
 bool buttonPressed[numberOfButtons] = {0, 0, 0, 0};
-int currentPos = 0;
+int currentPos;
 #define MATRIXPIN 6 
 #define NUMPIXELS 64
 
@@ -29,42 +30,67 @@ for (int i=0;i<numberOfButtons;i++){
     pinMode(buttonPins[i], INPUT_PULLUP);
 }
 randomSeed(analogRead(0));
-randomNumber = random(0, NUMPIXELS);
 
+currentMaze = random(0,9);
+currentPos = getPosition();
+while(currentPos == circlePos[currentMaze][0] || currentPos == circlePos[currentMaze][1]){
+    currentPos = getPosition();
+}
+finalPosition = getPosition(currentPos);
 Serial.begin(9600);
-Serial.println("Starting");
-matrix.setPixelColor(currentPos, 255, 0, 0);
-matrix.setPixelColor(randomNumber, 0, 255, 0);
+Serial.println("Starting:");
+Serial.print("Current Maze: ");
+Serial.println(currentMaze);
+Serial.print("Final Position: ");
+Serial.println(finalPosition);
+matrix.setPixelColor(currentPos, 50, 50, 50);
+matrix.setPixelColor(finalPosition, 50, 0, 0);
+//matrix.setPixelColor(randomNumber, 0, 255, 0);
+for (int i=0; i<2;i++){
+    matrix.setPixelColor(circlePos[currentMaze][i], 0, 50, 0);
+}
+fillEdges(matrix);
 matrix.show();
-
 }
 
 void loop(){
+
 for (int i=0; i<numberOfButtons;i++){
     buttonPressed[i] = checkButtonPressed(buttonPins[i], buttonStates[i], lastButtonStates[i], lastDebounceTime[i]);
     if (buttonPressed[i]){
         if (i == 0){
-            moveLight(matrix, currentPos, 1);
+            moveLight(matrix, currentPos, 1,currentMaze);
         }
         else if (i == 1){
-            moveLight(matrix, currentPos, 8);
+            moveLight(matrix, currentPos, 8,currentMaze);
         }
         else if (i == 2){
-            moveLight(matrix, currentPos, -1);
+            moveLight(matrix, currentPos, -1,currentMaze);
         }
         else if (i == 3){
-            moveLight(matrix, currentPos, -8);
+            moveLight(matrix, currentPos, -8,currentMaze);
         }
-        if (currentPos == randomNumber){
-            Serial.println("You win!");
-            matrix.setPixelColor(randomNumber, 255, 0, 0);
-            matrix.show();
-            delay(1000);
-            randomNumber = random(0, NUMPIXELS);
-            matrix.setPixelColor(randomNumber, 0, 255, 0);
-            matrix.show();
         }
+        matrix.show();
+        
     }
-}
-
+    if (currentPos==finalPosition){
+        Serial.println("You found the final position");
+        for (int i=0; i<2;i++){
+            matrix.setPixelColor(circlePos[currentMaze][i], 0, 0, 0);
+        }
+        currentMaze = random(0,9);
+        for (int i=0; i<2;i++){
+            matrix.setPixelColor(circlePos[currentMaze][i], 0, 50, 0);
+        }
+        matrix.setPixelColor(currentPos, 0, 0, 0);
+        currentPos = circlePos[currentMaze][0];
+        while(currentPos == circlePos[currentMaze][0] || currentPos == circlePos[currentMaze][1])
+        {
+            currentPos = getPosition();
+        }
+        finalPosition=getPosition(currentPos);
+        matrix.setPixelColor(currentPos, 50, 50, 50);
+        matrix.setPixelColor(finalPosition, 50, 0, 0);  
+    }
 }
